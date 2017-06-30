f4d08ceb10f7f4583d564e72ce7b9a338c0916ef
JenkinsY94 <jinkaiyu94@gmail.com>
2017-06-30 15:12:09 +0800

    add model 2

--- yandex_ranking/main.py
+++ yandex_ranking/main.py
@@ -11,13 +11,15 @@ from preprocess import Session
 from tqdm import tqdm  # progress bar
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import log_loss
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import log_loss, mean_squared_error
 
 print(__doc__)
 
-NUM_LINES = 1*1e+4  # about 1e+8 lines in train file in total
+NUM_LINES = 1 * 1e+5  # about 1e+8 lines in train file in total
 TRAIN_DIR = 'input/train.gz'
-SUPPORT_THRESH = 2
+SUPPORT_THRESH = 5  # support threshold for category in one-hot feature construction.
+N_FEATURES = 2 ** 10  # feature dimension for hashing features.
 
 # step 1: preparing data
 # construct sessions.
@@ -36,18 +38,19 @@ with gzip.open(TRAIN_DIR, 'r') as f_train:
         else:
             raise ValueError("cannot resolve this line: \n%s" % line)
 
-        if idx+1 == NUM_LINES:
+        if idx + 1 == NUM_LINES:
             break
 print("session example:\n%s" % sessions[-2].to_string())
 print("#session read: %d" % len(sessions))
 
 # construct features from sessions.
 # consider using collections.queue for storing sessions, to improve efficiency.
-categories = Session.gen_category(sessions, sup_thresh=SUPPORT_THRESH)
+# categories = Session.gen_category(sessions, sup_thresh=SUPPORT_THRESH)
 X, y = None, None
 valid_samples = 0  # record current sample number in X
 for s in tqdm(sessions):
-    new_x, new_y = s.gen_feature(categories)
+    # new_x, new_y = s.gen_feature(categories)
+    new_x, new_y = s.gen_hash_feature(n_features=N_FEATURES)
     if new_x is None or new_y is None:
         continue
     if X is None:
@@ -56,8 +59,8 @@ for s in tqdm(sessions):
         while valid_samples + new_x.shape[0] > X.shape[0]:
             X.resize((2 * X.shape[0], X.shape[1]))
             y.resize(2 * y.shape[0])
-        X[valid_samples:valid_samples+new_x.shape[0]] = new_x
-        y[valid_samples:valid_samples+new_x.shape[0]] = new_y
+        X[valid_samples:valid_samples + new_x.shape[0]] = new_x
+        y[valid_samples:valid_samples + new_x.shape[0]] = new_y
     valid_samples += new_x.shape[0]
 X = X[:valid_samples]
 y = y[:valid_samples]
@@ -70,19 +73,28 @@ input("press enter to start training...")
 rounds = 5
 seed = 12345
 rng = np.random.RandomState(seed)
-clf = LogisticRegression(penalty='l2', solver='liblinear', C=1.0)
+clf_1 = LogisticRegression(penalty='l2', solver='liblinear', C=1.0, verbose=1)
+clf_2 = GradientBoostingRegressor(n_estimators=20, learning_rate=0.1, max_depth=10, loss='ls', verbose=1)
 all_acc, all_loss = [], []
-for r in range(rounds):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=rng)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)  # predict class label
-    acc = np.mean(y_test == y_pred)
-    y_pred_proba = clf.predict_proba(X_test)
-    loss = log_loss(y_test, y_pred_proba, labels=[0, 1])
-    print("round %d, accuracy: %f, log_loss: %f" % (r+1, acc, loss))
-    all_acc.append(acc)
-    all_loss.append(loss)
-print("average accuracy: %f, average loss: %f" % (np.mean(all_acc), np.mean(all_loss)))
 
+# for r in range(rounds):
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=rng)
+clf_1.fit(X_train, y_train)
+y_pred = clf_1.predict(X_test)  # predict class label
+acc = np.mean(y_test == y_pred)
+y_pred_proba = clf_1.predict_proba(X_test)
+loss = log_loss(y_test, y_pred_proba, labels=[0, 1])
+# print("round %d, accuracy: %f, log_loss: %f" % (r + 1, acc, loss))
+all_acc.append(acc)
+all_loss.append(loss)
+
+# step 3: train Model 2 (GBDT) and evaluate
+y_abs_err = np.abs(y_test - y_pred_proba[:, 1])
+clf_2.fit(X_test, y_abs_err)
+y_pred_2 = clf_2.predict(X_test)
+mse = mean_squared_error(y_test, y_pred_2)
+
+print("\nSummary of M1(LR):\n average accuracy: %f(std: %f)\n average loss: %f(std: %f)" \
+      % (np.mean(all_acc), np.std(all_acc), np.mean(all_loss), np.std(all_loss)))
+print("\nSummary of M2(GBDT):\n mse: %f" % mse)
 # step 3:
-
