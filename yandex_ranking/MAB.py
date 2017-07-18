af984565447279048556c5ae7f31dfff2a65875e
JenkinsY94 <jinkaiyu94@gmail.com>
2017-07-18 14:24:56 +0800

    add thompson sample over pos_score class

--- yandex_ranking/MAB.py
+++ yandex_ranking/MAB.py
@@ -6,10 +6,10 @@ import numpy as np
 class TSOverScore(object):
     """ Thompson Sampling over scores policy."""
     def __init__(self, bucket_size=100, a=1, b=1):
-        """ for each arm/bucket, initialize them with beta(1,1), i.e. uniform distribution.
-        :param bucket_size:
-        :param a: beta prior distribution param a.
-        :param b: beta prior distribution param b.
+        """
+        :param bucket_size: number of buckets.
+        :param a & b: beta prior distribution param. for each arm/bucket,
+                      initialize them with beta(1,1) by default, i.e. uniform distribution.
         """
         self.bucket_size = bucket_size
         self.buckets = [{'a': a, 'b': b} for i in range(bucket_size)]
@@ -18,10 +18,10 @@ class TSOverScore(object):
 
     def thompson_sample(self, pred_ctr, k, max_pos, weight_type='propensity'):
         """
-        :param pred_ctr: a list of predicted ctr
+        :param pred_ctr: list of predicted ctr
         :param k: position to explore.(count from 1)
         :param max_pos: explore candidate's maximum position.(count from 1)
-        :param weight_type: 'propensity' or 'multinomial'
+        :param weight_type: 'propensity' or 'multinomial'. otherwise assign unit weight.
         :return: index of item to explore (index count from 0);
                   index of bucket/arm (count from 0);
                   the chosen arm's weight (propensity weight or multinomial weight.)
@@ -31,7 +31,7 @@ class TSOverScore(object):
         for i, score in enumerate(pred_ctr[k-1: max_pos]):
             assert 0 <= score <= 1
             temp = dict()
-            temp['exp_idx'] = i+k-1
+            temp['exp_idx'] = i + k - 1
             bucket_idx = int(score * self.bucket_size) % self.bucket_size
             temp['bucket_idx'] = bucket_idx
             temp['ts_score'] = beta.rvs(self.buckets[bucket_idx]['a'], self.buckets[bucket_idx]['b'])
@@ -84,8 +84,7 @@ class TSOverPosition(object):
         """
         :param k: start position for explore and replace.(index from 1)
         :param max_pos: explore candidate's maximum position.(index from 1)
-        :param a: beta prior distribution param a.
-        :param b: beta prior distribution param b.
+        :param a & b: beta prior distribution param
         """
         self.valid_bucket_size = max_pos - k + 1
         self.buckets = [{'a': a, 'b': b} for i in range(max_pos)]
@@ -118,3 +117,77 @@ class TSOverPosition(object):
             self.buckets[bucket_idx]['b'] += 1
         else:
             raise ValueError("label must be either 0 or 1.")
+
+
+class TSOverPosScore(object):
+    """Thompson Sampling over position & score policy."""
+    def __init__(self, bucket_size=100, k=5, max_pos=10, a=1, b=1):
+        """
+        :param bucket_size: size of bucket/arm for each candidate position.
+        :param k: start position for explore and replace.(index from 1)
+        :param max_pos: explore candidate's maximum position.(index from 1)
+        :param a & b: beta prior distribution param
+        """
+        self.bucket_size = bucket_size
+        self.n = max_pos - k + 1
+        self.buckets = []
+        self.exp_times_each = []
+        for i in range(self.n):
+            self.buckets.append([{'a': a, 'b': b} for i in range(bucket_size)])
+            self.exp_times_each.append([0 for i in range(bucket_size)])
+        self.exp_times = 0
+
+    def thompson_sample(self, pred_ctr, k, max_pos, weight_type='propensity'):
+        """
+        :param pred_ctr: list of predicted ctr
+        :param k: position to explore.(count from 1)
+        :param max_pos: explore candidate's maximum position.(index from 1)
+        :param weight_type: 'propensity' or 'multinomial'. otherwise assign unit weight.
+        :return: index of item to explore (index count from 0);
+                  index of bucket/arm (count from 0);
+                  the chosen arm's weight (propensity weight or multinomial weight.)
+        """
+        assert 10 >= max_pos > k > 1
+        active_arms = []
+        for i, score in enumerate(pred_ctr[k-1: max_pos]):
+            assert 0 <= score <= 1
+            temp = dict()
+            temp['exp_idx'] = i + k - 1
+            bucket_idx = int(score * self.bucket_size) % self.bucket_size
+            temp['bucket_idx'] = (i, bucket_idx)  # 2-level index
+            temp['ts_score'] = beta.rvs(self.buckets[i][bucket_idx]['a'], self.buckets[i][bucket_idx]['b'])
+            active_arms.append(temp)
+        arm_chosen = max(active_arms, key=lambda x: x['ts_score'])
+        self.exp_times += 1
+        self.exp_times_each[arm_chosen['bucket_idx'][0]]\
+                           [arm_chosen['bucket_idx'][1]] += 1
+
+        if weight_type == 'propensity':
+            weight = 1.0 * self.exp_times / self.exp_times_each[arm_chosen['bucket_idx'][0]][arm_chosen['bucket_idx'][1]]
+        elif weight_type == 'multinomial':
+            weight = 1.0 * sum(pred_ctr[k-1: max_pos]) / pred_ctr[arm_chosen['exp_idx']]
+        else:
+            weight = 1
+        return arm_chosen['exp_idx'], arm_chosen['bucket_idx'], weight
+
+    def batch_update(self, bucket_idx, label):
+        if label == 1:
+            self.buckets[bucket_idx[0]][bucket_idx[1]]['a'] += 1
+        elif label == 0:
+            self.buckets[bucket_idx[0]][bucket_idx[1]]['b'] += 1
+        else:
+            raise ValueError("label must be either 0 or 1.")
+
+    def show_explore_times(self):
+        plt.title("explore times of each bucket(TS over score)")
+        for i in range(self.n):
+            plt.subplot(self.n, 1, i+1)
+            plt.bar(np.arange(self.bucket_size), self.exp_times_each[i])
+            plt.xlabel("bucket index")
+            plt.ylabel("times")
+        plt.show()
+
+
+class TSOverMae(object):
+
+    pass
